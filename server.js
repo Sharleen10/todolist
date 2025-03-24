8b43ec7d6ecc431036d79916c294c2eaf6d385d0
Sharleen10 <sharleensshabangu@gmail.com>
2025-03-24 19:29:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/tasks', taskRoutes);
+
+// Serve the main app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
